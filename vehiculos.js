d3a6d30bd641caf416b70c8694a3d40b4dc50eeb
HaleymHidalgo <haleymgabrielhidalgo@gmail.com>
2024-05-30 20:10:55 -0400

    update registros

--- vehiculos.js
+++ vehiculos.js
@@ -3,106 +3,190 @@
 module.exports = [
     {
         "id": 0,
-        "marca": "Suzuki",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1682716203-JkRG8FACQ3.webp",
+        "marca": "suzuki",
         "modelo": "Nuevo S-presso",
         "precio": "8.040.000",
-        "cantidad": 35
+        "cantidad": 35,
+        "descripcion":"El Nuevo Suzuki S-presso, vuelve con más razones para hablar de él. Su diseño inspirado en los icónicos SUV de Suzuki le entrega una personalidad única. Su look prominente, robusto y enérgico, le permite ser reconocido de forma instantánea en las calles. Además, su excepcional rendimiento de combustible lo convierte en uno de los modelos más eficientes de Suzuki. Descubre el Suzuki S·PRESSO, el primer SUV compacto de Suzuki.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1682716203-JkRG8FACQ3.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1683324000-hWH9Vh38pM.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1683324015-nE7jwViZpZ.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1683324194-RYBzf3qVWb.webp",
+        ]
     },
     {
         "id": 1,
-        "marca": "Suzuki",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/2022-05-25-FOTOAUTO-DESCRIPCIÓN-FICHAMODELO-754X364.png",
+        "marca": "suzuki",
         "modelo": "Cerelio",
         "precio": "8.940.000",
-        "cantidad": 57
+        "cantidad": 57,
+        "descripcion":"Entra en el espíritu de Suzuki Celerio y experimenta la sensación de libertad cada vez que estás detrás del volante. Vive nuevas experiencias arriba de un auto que lo tiene todo: eficiencia de combustible, diseño y comodidad. Si buscas entretenimiento sin sacrificar rendimiento, Celerio es para ti.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/2022-05-25-FOTOAUTO-DESCRIPCIÓN-FICHAMODELO-754X364.png",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/2022-05-26-FOTOEXTERIOR-1440X810-4.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/2022-05-26-FOTOEXTERIOR-1440X810-5.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/2022-05-26-FOTOEXTERIOR-1440X810-1.jpg"
+        ]
     },
     {
         "id": 2,
-        "marca": "Suzuki",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/2021-06-25-swift-plus_754x364.png",
+        "marca": "suzuki",
         "modelo": "Swift",
         "precio": "9.640.000",
-        "cantidad": 145
+        "cantidad": 145,
+        "descripcion": "Atrévete a hacer algo nuevo con un modelo que está preparado para cualquier panorama. Un icónico de Suzuki que entrega diseño atractivo, rendimiento incomparable, amplio maletero y gran conectividad para llegar a cualquier destino, sin perder tu personalidad deportiva.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/2021-06-25-swift-plus_754x364.png",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/2021-09-21-WEB_EXTERIOR-SWIFT_DESKTOP-1200x700-4.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/2021-09-21-WEB_EXTERIOR-SWIFT_DESKTOP-1200x700-2.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/2021-09-21-WEB_EXTERIOR-SWIFT_DESKTOP-1200x700-extra%201.jpg"
+        ]
     },
     {
         "id": 3,
-        "marca": "Suzuki",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1659392017-yrZC3CplcJ.webp",
+        "marca": "suzuki",
         "modelo": "Dzire",
         "precio": "9.740.000",
-        "cantidad": 94
+        "cantidad": 94,
+        "descripcion":"Súbete a un modelo con actitud de hatchback, pero con el espacio que buscas en un sedán. Su personalidad y rendimiento eficiente te permitirán seguir siendo tú, aunque los caminos cambien. Libera aventuras para ti y tu familia, gracias a su espacio interior y capacidad del maletero.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1659392017-yrZC3CplcJ.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/suzuki/migration/outdoor-image/dzire/suzuki-nuevo-dzire-galleria-exterior-02.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/suzuki/migration/outdoor-image/dzire/suzuki-nuevo-dzire-galleria-exterior-05.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/suzuki/migration/outdoor-image/dzire/suzuki-nuevo-dzire-galleria-exterior-04.jpg"
+        ]
     },
     {
         "id": 4,
-        "marca": "Suzuki",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1703779969-JpwQJECso6.webp",
+        "marca": "suzuki",
         "modelo": "Alto",
         "precio": "7.690.000",
-        "cantidad": 24
+        "cantidad": 24,
+        "descripcion":"Ahora mas amplio y cómodo, con espacio para todos los panoramas familiares. Confianza, funcionalidad y eficiencia. El Nuevo Suzuki Alto es el city perfecto para acompañarte en todas; gran conectividad, más capacidad y rendimiento como ninguno.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/models/1703779969-JpwQJECso6.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/1703779575-Fl6nTRvSNp.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/1703779575-Fl6nTRvSNp.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/1703779614-biK6w4hh9L.webp"
+        ]
+        
     },
     {
         "id": 5,
-        "marca": "Mazda",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1710178849-bJ745ZwgoY.webp",
+        "marca": "mazda",
         "modelo": "Sedán",
         "precio": "15.490.000",
-        "cantidad": 69
+        "cantidad": 69,
+        "descripcion":"Atraer las miradas de todos, es algo que muchos quieren pero que pocos consiguen, como el New Mazda2 Sedán. Su diseño KODO “Alma del Movimiento” te entrega la seguridad que buscas a través de un diseño totalmente atractivo. ",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1710178849-bJ745ZwgoY.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/mazda/migration/outdoor-image/mazda2-sedan/galeria_img_1_exterior-2.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/mazda/migration/outdoor-image/mazda2-sedan/galeria_img_4_exterior-2.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/mazda/migration/outdoor-image/mazda2-sedan/galeria_img_3_exterior-2.jpg"
+        ]
+        
     },
     {
         "id": 6,
-        "marca": "Mazda",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1710178083-u9ppaOw4VR.webp",
+        "marca": "mazda",
         "modelo": "Sport",
         "precio": "18.290.000",
-        "cantidad": 96
+        "cantidad": 96,
+        "descripcion":"Estar a la vanguardia nunca ha sido fácil, para All-New Mazda3 Sport sí lo es. Un auto que atrae e inspira confianza y seguridad con toda su tecnología y estilo. Descúbrelo y verás cómo provoca. ",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1710178083-u9ppaOw4VR.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/1693944108-Lgw2WuULh1.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/1693944162-b1nWXLxG5X.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/carcontent/1693944149-C8XYRX62FX.webp"
+        ]
+        
     },
     {
         "id": 7,
-        "marca": "Mazda",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1701698750-6GluwSgq39.webp",
+        "marca": "mazda",
         "modelo": "CX-3",
         "precio": "18.490.000",
-        "cantidad": 70
+        "cantidad": 70,
+        "descripcion":"Siempre hay límites que alcanzar, nunca hay desafíos imposibles, New Mazda CX-3 lo demuestra con su diseño y tecnología de vanguardia. Descubre cada detalle en todas tus aventuras. ",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1701698750-6GluwSgq39.webp",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/mazda/migration/outdoor-image/new-mazda-cx-3/2019-mazda-cx-3-compact-car.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/mazda/migration/outdoor-image/new-mazda-cx-3/2019-mazda-cx-3-compact-crossover-soul-red.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/mazda/migration/outdoor-image/new-mazda-cx-3/2019-mazda-cx-3-compact-crossover_rear-view.jpg"
+        ]
+        
     },
     {
         "id": 8,
-        "marca": "Renault",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1671188694-UriXC7CCe3.webp",
+        "marca": "renault",
         "modelo": "Kwid",
         "precio": "8.490.000",
-        "cantidad": 210
+        "cantidad": 210,
+        "descripcion": "El SUV de los compactos. Más Diseño, Más Seguridad y Más Tecnología al mejor precio, encuéntralo en tu concesionario más cercano.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1671188694-UriXC7CCe3.webp",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/kwid/diseno_principal.jpg",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/kwid/diseno_3.jpg"
+        ]
+        
     },
     {
         "id": 9,
-        "marca": "Renault",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1708008893-q4gOGH08q3.webp",
+        "marca": "renault",
         "modelo": "Kwid E_TECH",
         "precio": "14.990.000",
-        "cantidad": 14
+        "cantidad": 14,
+        "descripcion":"Con el nuevo Renault Kwid eléctrico, la marca francesa espera democratizar los autos de entrada cero emisiones. El Kwid E-Tech 2024 llegó a Colombia en una única versión con una batería de 26.8 kWh de capacidad que promete una autonomía de hasta 298 km con una sola carga",
+        "img": [
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/e-kwid-tech/DISENO/EXTERIOR/DISENO-EXTERIOR-CARRUSEL5.jpg",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/e-kwid-tech/DISENO/EXTERIOR/DISENO-EXTERIOR-CARRUSEL8.jpg",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/e-kwid-tech/DISENO/EXTERIOR/DISENO-EXTERIOR-CARRUSEL.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1708008893-q4gOGH08q3.webp"
+        ]
+        
     },
     {
         "id": 10,
-        "marca": "Renault",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/versions/2022-01-26-Duster-llanta.jpg",
+        "marca": "renault",
         "modelo": "All New Duster",
         "precio": "15.390.000",
-        "cantidad": 53
+        "cantidad": 53,
+        "descripcion":"Prepárate para dejarte llevar por el diseño dinámico del nuevo Renault DUSTER, con su nueva máscara frontal y sus líneas expresivas, más modernas y robustas.",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/renault/migration/outdoor-image/all-new-duster/renault-newduster-diseno-exterior-1.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/versions/2022-01-26-Duster-llanta.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/renault/migration/outdoor-image/all-new-duster/renault-newduster-diseno-exterior-7.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/medias/renault/migration/outdoor-image/all-new-duster/renault-newduster-diseno-exterior-4.jpg"
+        ]
+        
     },
     {
         "id": 11,
-        "marca": "Renault",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1671188451-N4tLIqZfro.webp",
+        "marca": "renault",
         "modelo": "Nueva Oroch",
         "precio": "15.815.100",
-        "cantidad": 83
+        "cantidad": 83,
+        "descripcion":"La nueva Renault Oroch llega con la fuerza, robustez y versatilidad de una Pick Up, con un amplio portalón en todas sus versiones y un espacio interior perfecto para total confort.",
+        "img": [
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/nueva-oroch/diseno/disen%25CC%2583o-exterior.jpg",
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/version/1671188451-N4tLIqZfro.webp",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/nueva-oroch/diseno/diseno-exterior/nueva-oroch-galeria-exterior-1.jpg",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/nueva-oroch/diseno/diseno-exterior/nueva-oroch-galeria-exterior-2.jpg"
+        ]
+        
     },
     {
         "id": 12,
-        "marca": "Renault",
-        "img": "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/versions/2022-05-25-Express 4.jpg",
+        "marca": "renault",
         "modelo": "Nuevo Express",
         "precio": "16.053.000",
-        "cantidad": 95
+        "cantidad": 95,
+        "descripcion":"Con un amplio espacio interior, un panel con numerosos compartimentos, asientos con un diseño moderno. Todo lo necesario para que disfrutes de una experiencia a bordo cómoda y práctica. *pantalla disponible según versión",
+        "img": [
+            "https://dercocenter-api.s3.us-east-1.amazonaws.com/images/versions/2022-05-25-Express%204.jpg",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/nuevo-express/diseno/exterior/2.jpg",
+            "https://www.renault.cl/_ipx/_/https://s3.amazonaws.com/dercocenter.cl/brands/renault/nuevo-express/diseno/exterior/PORTADA.jpg"
+        ]
     }
 ]
